117202292516e71ecccd864c2c33bf1f669d0ccf
Aryan <rathodaryan1718@gmail.com>
2026-09-20 15:34:13 +0530

    Implement Practical 2 useState and styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,19 +2,49 @@ import { useState } from "react";
 
 function Contact() {
   const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
 
   return (
     <section className="card">
       <h2>Contact</h2>
 
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+            setSubmitted(false);
+          }}
+        />
+
+        <input
+          type="text"
+          placeholder="Enter your message"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+            setSubmitted(false);
+          }}
+        />
+
+        <button type="submit">Send Message</button>
+      </form>
 
       <p>Hello, {name}</p>
+
+      {submitted && (
+        <p>
+          Message sent successfully!
+        </p>
+      )}
     </section>
   );
 }
